679f3c4aafbbe5a1ae89b231f2bba896b699db0a
guiaixiao <824541050@qq.com>
2019-04-09 16:29:53 +0800

    vue instance

---
+++ chapter 1/main.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    name:'myVueApp',
+    el: '#root',
+    data () {
+        return {
+            messages:'Hello Vue.js!',
+        }
+    },
+})
+
+
+var app2 = new Vue({
+    data () {
+        return {
+            messages:'Hello,$ function!'
+        }
+    },
+})
+
+app2.$mount('#root2')  // Vue实例的 $mount方法代替 el选项
